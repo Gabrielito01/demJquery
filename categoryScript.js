bfe51ae03352979408c8afed514ae92fdf195fb0
Gabrielito01 <50943002+Gabrielito01@users.noreply.github.com>
2020-05-14 17:47:23 +0200

    Update categoryScript.js

--- categoryScript.js
+++ categoryScript.js
@@ -195,11 +195,10 @@
       <div class="container">
         <p class="m-0 text-center text-white">Copyright © Gabrielito Website 2019</p>
       </div>
-      <!-- /.container -->
     </footer>
     </div>
-   </div>
   
+      <!-- /.container -->
     <!-- Bootstrap core JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
